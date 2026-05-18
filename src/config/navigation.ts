fe831e4f82f83d781dd0446d8644b842ddaaaf53
Luke Boyett <luke@sineworks.io>
2026-05-18 11:51:48 -0500

    change href in navbar

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,16 +1,9 @@
 import type { NavLink } from "../types";
 
-const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
+const basePath = import.meta.env.BASE_URL;
 
 export const NAV_LINKS: NavLink[] = [
-  { href: `${basePath}/`, label: "About", isActive: true },
+  { href: basePath, label: "About", isActive: true },
   { href: `${basePath}/dream-casa`, label: "Dream Casa", isActive: true },
   { href: `${basePath}/blue-bungalow`, label: "Blue Bungalow", isActive: true },
-  //   { href: "/publications", label: "Publications", isActive: true },
-  //   { href: "/talks", label: "Talks", isActive: true },
-  //   { href: "/teaching", label: "Teaching", isActive: true },
-  //   { href: "/projects", label: "Code", isActive: true },
-  //   { href: "/posts", label: "Blog", isActive: true },
-  //   { href: "/tags", label: "Tags", isActive: true },
-  //   { href: "/cv", label: "CV", isActive: true },
 ];
